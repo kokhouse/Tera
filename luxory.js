19cba5e67eb8e6ef06a1ba4233d8e30b5fa946c4
luxory <dovescrymgmt@gmail.com>
2017-05-18 10:46:49 -0400

    bro

--- luxory.js
+++ luxory.js
@@ -16,43 +16,43 @@ console.log("Starting DiscordBot\nNode version: " + process.version + "\nDiscord
 
 
 // Init AWS S3
-var AWS = require('aws-sdk');
-var s3 = new AWS.S3();
-
-function downloadConfig(config){
-	return new Promise(function (fulfill, reject){
-		s3.getObject({
-			Bucket: process.env.S3_BUCKET_NAME,
-			Key: config + '.json'
-		}, function(err, data){
-			if(err){
-				console.log(err);
-				reject(err);
-			} else {
-				console.log('Downloaded ' + config + '.json, size: ' + data.Body.length);
-				fulfill(JSON.parse(data.Body.toString()));
-			}
-		});
-	})
-}
-
-function uploadConfig(config){
-	var filename = './' + config + '.json';
-	var fileBuffer = require('fs').readFileSync(filename);
+//var AWS = require('aws-sdk');
+//var s3 = new AWS.S3();
+//
+//function downloadConfig(config){
+//	return new Promise(function (fulfill, reject){
+//		s3.getObject({
+//			Bucket: process.env.S3_BUCKET_NAME,
+//			Key: config + '.json'
+//		}, function(err, data){
+//			if(err){
+//				console.log(err);
+//				reject(err);
+//			} else {
+//				console.log('Downloaded ' + config + '.json, size: ' + data.Body.length);
+//				fulfill(JSON.parse(data.Body.toString()));
+//			}
+//		});
+//	})
+//}
+//
+//function uploadConfig(config){
+//	var filename = './' + config + '.json';
+//	var fileBuffer = require('fs').readFileSync(filename);
 	//var metaData = getContentTypeByFile(filename);
-
-	s3.putObject({
-		Bucket: process.env.S3_BUCKET_NAME,
-		Key: config + '.json',
-		Body: fileBuffer
-	}, function(err, data){
-		if(err){
-			console.log('File upload failed for ' + filename);
-			console.log(err);
-		} else {
-			console.log('Uploaded file ' + filename + ' to the cloud.');
-		}
-	});
+//
+//	s3.putObject({
+//		Bucket: process.env.S3_BUCKET_NAME,
+//		Key: config + '.json',
+//		Body: fileBuffer
+//	}, function(err, data){
+//		if(err){
+//			console.log('File upload failed for ' + filename);
+//			console.log(err);
+//		} else {
+//			console.log('Uploaded file ' + filename + ' to the cloud.');
+//		}
+//	});
 }
 // Load configuration and other assets
 var Config = downloadConfig('config');
