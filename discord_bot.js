3eb6c39dc527d218d78a134d14a34c6f558be362
luxory <dovescrymgmt@gmail.com>
2017-05-17 23:40:12 -0400

    stranger danger

--- discord_bot.js
+++ discord_bot.js
@@ -1,56 +1,29 @@
-// Init Requirements
-var env = require('dotenv').config();
-var fs = require('fs');
-var Promise = require('promise');
-var request = require('request-promise');
 
-try {
-	var Discord = require("discord.js");
-} catch (e){
-	console.log(e.stack);
-	console.log(process.version);
-	console.log("hmm surely u didn't do something correct pallo!");
-	process.exit();
+function run(){
+// Load permissions
+var dangerousCommands = ["eval","pullanddeploy","setUsername", "permit", "gpermit", "setkey", "getkey"];
+
+for( var i=0; i<dangerousCommands.length;i++ ){
+	var cmd = dangerousCommands[i];
+	if(!Permissions.global.hasOwnProperty(cmd)){
+		Permissions.global[cmd] = false;
+	}
 }
-console.log("Starting DiscordBot\nNode version: " + process.version + "\nDiscord.js version: " + Discord.version);
-
-
-// Init AWS S3
-var AWS = require('aws-sdk');
-var s3 = new AWS.S3();
-
-function downloadConfig(config){
-	return new Promise(function (fulfill, reject){
-		s3.getObject({
-			Bucket: process.env.S3_BUCKET_NAME,
-			Key: config + '.json'
-		}, function(err, data){
-			if(err){
-				console.log(err);
-				reject(err);
-			} else {
-				console.log('Downloaded ' + config + '.json, size: ' + data.Body.length);
-				fulfill(JSON.parse(data.Body.toString()));
+Permissions.checkPermission = function (user,permission){
+	try {
+		var allowed = true;
+		try{
+			if(Permissions.global.hasOwnProperty(permission)){
+				allowed = Permissions.global[permission] === true;
 			}
-		});
-	})
+		} catch(e){}
+		try{
+			if(Permissions.users[user.id].hasOwnProperty(permission)){
+				allowed = Permissions.users[user.id][permission] === true;
+			}
+		} catch(e){}
+		return allowed;
+	} catch(e){}
+	return false;
 }
 
-function uploadConfig(config){
-	var filename = './' + config + '.json';
-	var fileBuffer = require('fs').readFileSync(filename);
-	//var metaData = getContentTypeByFile(filename);
-
-	s3.putObject({
-		Bucket: process.env.S3_BUCKET_NAME,
-		Key: config + '.json',
-		Body: fileBuffer
-	}, function(err, data){
-		if(err){
-			console.log('File upload failed for ' + filename);
-			console.log(err);
-		} else {
-			console.log('Uploaded file ' + filename + ' to the cloud.');
-		}
-	});
-}
